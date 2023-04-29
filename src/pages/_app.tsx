a930307a47179e42369c7bfe886ea18b640fd875
Kevin Ibanez <kpeibanez@gmail.com>
2023-04-28 20:14:22 -0500

    started implementing login features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from '@clerk/nextjs';
+import {dark} from '@clerk/themes';
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    <ClerkProvider appearance={{baseTheme: dark}} {...pageProps} >
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
